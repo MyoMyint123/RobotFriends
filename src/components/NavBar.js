b23576d75387d13558d28027886f972d4a01af82
MyoMyint123 <myomyint117@gmail.com>
2021-01-27 12:31:20 +0630

    Change Labels for Menu Link and Brand text

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => (
     <nav className="ui raised very padded segment" id="nav-bar">
-        <Link className="ui inverted segment" to="/">React Tutorials</Link>
+        <Link className="ui inverted segment" to="/">React Redux Training</Link>
         <div className="ui right floated header">
             <NavLink className="ui button" to="/">Home</NavLink>
             <NavLink className="ui button" to="/users">Api Tutorial</NavLink>
-            <NavLink className="ui button" to="/to-do-list">To Do List Tutorial </NavLink>
+            <NavLink className="ui button" to="/to-do-list"> ToDoList </NavLink>
         </div>
     </nav>
 );
